68f98d810e18e06ec7345ae61cad7c890f02e103
Fangjun Kuang <csukuangfj@gmail.com>
2023-10-27 17:55:44 +0800

    update lexicon

--- .github/scripts/additional_words.py
+++ .github/scripts/additional_words.py
@@ -2,6 +2,12 @@ def get_additional_english_words():
     words = [
         "ceo",
         "sandra's",
+        "ganjaj",
+        "qataris",
+        "qatar's",
+        "pandubi",
+        "mithu" "bhargav",
+        "narendra",
     ]
     return words
 
