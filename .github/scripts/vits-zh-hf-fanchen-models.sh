2578488ef424e407f1045ded519ae4d38f3494fc
Fangjun Kuang <csukuangfj@gmail.com>
2023-11-07 14:11:37 +0800

    small fixes

--- .github/scripts/vits-zh-hf-fanchen-models.sh
+++ .github/scripts/vits-zh-hf-fanchen-models.sh
@@ -6,6 +6,7 @@ echo "name: $NAME"
 
 pip install unidecode onnx onnxruntime pyopenjtalk jamo \
   Cython scipy \
+  jieba \
   ko_pron pypinyin cn2an indic_transliteration eng_to_ipa num_thai \
   torch==1.13.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
 
