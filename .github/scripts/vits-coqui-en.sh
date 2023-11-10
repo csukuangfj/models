472dbd389281ca987c3ce9368e170e362ec51b29
Fangjun Kuang <csukuangfj@gmail.com>
2023-11-10 19:27:41 +0800

    add vits-coqui

--- .github/scripts/vits-coqui-en.sh
+++ .github/scripts/vits-coqui-en.sh
@@ -11,14 +11,16 @@ if [[ $name == "ljspeech-vits" ]]; then
 elif [[ $name == "ljspeech-vits-neon" ]]; then
   wget -q https://coqui.gateway.scarf.sh/v0.8.0_models/tts_models--en--ljspeech--vits.zip
   unzip tts_models--en--ljspeech--vits.zip
-  mv -v tts_models--en--ljspeech--vits/model_file.pth.tar ../model_file.pth
+  mv -v tts_models--en--ljspeech--vits/model_file.pth.tar ./model_file.pth
   mv -v tts_models--en--ljspeech--vits/config.json ./
 elif [[ $name == "vctk-vits" ]]; then
   wget -q https://coqui.gateway.scarf.sh/v0.6.1_models/tts_models--en--vctk--vits.zip
   unzip tts_models--en--vctk--vits.zip
   mv -v tts_models--en--vctk--vits/config.json ./
-  mv -v tts_models--en--vctk--vits/speaker_ids.json ./
+  cp -v tts_models--en--vctk--vits/speaker_ids.json ./
   mv -v tts_models--en--vctk--vits/model_file.pth ./
+  mkdir -p /home/julian/.local/share/tts/tts_models--en--vctk--vits
+  cp -v speaker_ids.json /home/julian/.local/share/tts/tts_models--en--vctk--vits
 elif [[ $name == "jenny-jenny" ]]; then
   wget -q https://coqui.gateway.scarf.sh/v0.14.0_models/tts_models--en--jenny--jenny.zip
   unzip tts_models--en--jenny--jenny.zip
