76f4e06e0e748a1820c75021eb473306835ad1f1
Fangjun Kuang <csukuangfj@gmail.com>
2023-11-14 10:27:45 +0800

    add more french models

--- .github/scripts/vits-piper-fr_FR.sh
+++ .github/scripts/vits-piper-fr_FR.sh
@@ -8,9 +8,34 @@ echo "type: $type"
 
 set -ex
 
-wget -qq https://huggingface.co/rhasspy/piper-voices/resolve/main/fr/fr_FR/$name/$type/fr_FR-$name-$type.onnx
-wget -qq https://huggingface.co/rhasspy/piper-voices/resolve/main/fr/fr_FR/$name/$type/fr_FR-$name-$type.onnx.json
-wget -qq https://huggingface.co/rhasspy/piper-voices/resolve/main/fr/fr_FR/$name/$type/MODEL_CARD
+if [[ $name == "tjiho" ]]; then
+  if [[ $type == "model1" ]]; then
+    wget -qq "https://raw.githubusercontent.com/tjiho/French-tts-model-piper/main/model%201/tom1.onnx.json"
+    wget -qq "https://raw.githubusercontent.com/tjiho/French-tts-model-piper/main/model%201/tom1.onnx"
+    mv -v tom1.onnx fr_FR-$name-$type.onnx
+    mv -v tom1.onnx.json fr_FR-$name-$type.onnx.json
+  elif [[ $type == "model2" ]]; then
+    wget -qq "https://raw.githubusercontent.com/tjiho/French-tts-model-piper/main/model%202/tom2.onnx.json"
+    wget -qq "https://raw.githubusercontent.com/tjiho/French-tts-model-piper/main/model%202/tom2.onnx"
+
+    mv -v tom2.onnx fr_FR-$name-$type.onnx
+    mv -v tom2.onnx.json fr_FR-$name-$type.onnx.json
+  elif [[ $type == "model3" ]]; then
+    wget -qq "https://raw.githubusercontent.com/tjiho/French-tts-model-piper/main/model%203/tom3.onnx.json"
+    wget -qq "https://raw.githubusercontent.com/tjiho/French-tts-model-piper/main/model%203/tom3.onnx"
+    mv -v tom3.onnx fr_FR-$name-$type.onnx
+    mv -v tom3.onnx.json fr_FR-$name-$type.onnx.json
+  else
+    echo "unknown type: $type for name: $name"
+    exit 1
+  fi
+
+  echo "This model is converted from https://github.com/tjiho/French-tts-model-piper" > MODEL_CARD
+else
+  wget -qq https://huggingface.co/rhasspy/piper-voices/resolve/main/fr/fr_FR/$name/$type/fr_FR-$name-$type.onnx
+  wget -qq https://huggingface.co/rhasspy/piper-voices/resolve/main/fr/fr_FR/$name/$type/fr_FR-$name-$type.onnx.json
+  wget -qq https://huggingface.co/rhasspy/piper-voices/resolve/main/fr/fr_FR/$name/$type/MODEL_CARD
+fi
 
 pip install piper-phonemize onnx onnxruntime==1.16.0
 
